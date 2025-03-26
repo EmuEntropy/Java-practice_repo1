f1ca6ae9e6b6daff594e3ae4df83bfc4bb5e5ac4
MK <maxkrawarik@outlook.com>
2025-03-26 13:30:14 -0700

    Moved CentStacker.java file location into Java-practice_repo1, Switched to java.util.Stack from algs4 lib stack

--- CentStacker.java
+++
@@ -1,7 +0,0 @@
-public class CentStacker {
-    public final int quarter = 25;
-    public final int dime = 10;
-    public final int nickel = 5;
-    public final int penny = 1;
-    public int total;
-}
---
+++ Java Practice Projects/CentStacker.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class CentStacker {
+    public final int quarter = 25;
+    public final int dime = 10;
+    public final int nickel = 5;
+    public final int penny = 1;
+    public int totalBalance;
+    Stack<Integer> pennies = new Stack<>();
+    Stack<Integer> nickels = new Stack<>();
+    Stack<Integer> dimes = new Stack<>();
+    
+}
+
+
