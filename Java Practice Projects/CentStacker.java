331f96a8c81a74a2d0d40fb77ab60ed0f9064292
MK <maxkrawarik@outlook.com>
2025-03-26 13:34:46 -0700

    implemented addPenny, removePenny, addNickels, removeNickel

--- Java Practice Projects/CentStacker.java
+++ Java Practice Projects/CentStacker.java
@@ -6,10 +6,47 @@ public class CentStacker {
     public final int nickel = 5;
     public final int penny = 1;
     public int totalBalance;
+    public int totalpoppedBalance;
     Stack<Integer> pennies = new Stack<>();
     Stack<Integer> nickels = new Stack<>();
     Stack<Integer> dimes = new Stack<>();
-    
+    Stack<Integer> quarters = new Stack<>();
+
+    // Method to add pennies to the stack
+    public void addPennies(int count) {
+        for (int i = 0; i < count; i++) {
+            pennies.push(penny);
+            totalBalance += penny;
+        }
+    }
+
+    // Method to remove a penny from the stack
+    public int removePenny() {
+        if (!pennies.isEmpty()) {
+            totalBalance -= pennies.pop();
+            return penny;
+        } else {
+            System.out.println("No pennies left to remove!");
+            return 0;
+        }
+    }
+
+    public void addNickels(int count) {
+        for (int i = 0; i < count; i++) {
+            nickels.push(nickel);
+            totalBalance += nickel;
+        }
+    }
+
+    public int removeNickel() {
+        if (!nickels.isEmpty()) {
+            totalBalance -= nickels.pop();
+            return nickel;
+        } else {
+            System.out.println("No nickels left to remove!");
+            return 0;
+        }
+    }
 }
 
 
